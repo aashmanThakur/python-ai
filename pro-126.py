cc52cb25284444f798318f1a37dcb288f91450f3
aashmanThakur <86097251+aashmanThakur@users.noreply.github.com>
2022-01-31 18:21:51 +0530

    Add files via upload

--- pro-126.py
+++ pro-126.py
@@ -1,39 +1,24 @@
-from email import header
-from selenium import webdriver
+
 from bs4 import BeautifulSoup
-import time,os,ssl
+import requests
+import pandas as pd
 import csv
 
-START_URL="https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
-
-if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
-    getattr(ssl, '_create_unverified_context', None)): 
-    ssl._create_default_https_context = ssl._create_unverified_context
-
-browser=webdriver.Chrome("chromedriver")
-
-browser.get(START_URL)
-time.sleep(10)
-
-def scrap():
-    headers = ["V-Mag","Proper name","Bayer-designation","distance","Spectral Class","Mass","Radius","luminosity"]
-    star_data=[]
-    soup = BeautifulSoup(browser.page_source, "html.parser")
-    for tr_tag in soup.find_all("tr"):
-            td_tags = tr_tag.find_all("td")
-            temp_list = []
-            for index, td_tag in enumerate(td_tags):
-                if index == 0:
-                    temp_list.append(td_tag.find_all("a")[0].contents[0])
-                else:
-                    try:
-                        temp_list.append(td_tag.contents[0])
-                    except:
-                        temp_list.append("")
-            star_data.append(temp_list)
-    with open("scrapper_2.csv", "w") as f:
+START_URL = (
+    "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+)
+wiki = requests.get(START_URL)
+soup = BeautifulSoup(wiki.text, "html.parser")
+temp_list = []
+for tr in soup.find("table").find_all("tr"):
+    td = tr.find_all("td")
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+headers=['name', 'distance', 'mass', 'radius']
+star_data=[]
+star_data.append(temp_list)
+print(star_data)
+with open("scrapper-stars.csv", "w") as f:
             csvwriter = csv.writer(f)
             csvwriter.writerow(headers)
-            csvwriter.writerows(star_data)
-    
-scrap()
+            csvwriter.writerows(temp_list)
